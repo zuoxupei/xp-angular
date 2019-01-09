7a0f46b83878b73e04981a5d822286acb77c6c07
zuoxupei <1432603542@qq.com>
2019-01-09 14:48:02 +0800

    zxp

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
+}else{
+  console.log("这里有你想要的一切")
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)

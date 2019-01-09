fdc48584a304381a758ddfdb27ae11106409dbc6
zuoxupei <1432603542@qq.com>
2019-01-09 15:10:08 +0800

    zxp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule, EVENT_MANAGER_PLUGINS } from '@angular/platform-browser'
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-import { IfwToastModule } from './core/lib/ifw-toast/ifw-toast.module';
 import { MultiEventPlugin } from './core/plugins/MultiEventPlugin';
 import { IErrorHandler } from './core/monitor/IErrorHandler';
 import { AppRouteModule } from './route/app-route.module';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { DirectiveModuleModule } from './core/directive/directive-module.module';
+import { IfwToastModule } from './core/component/ifw-toast/ifw-toast.module';
 
 @NgModule({
   declarations: [
